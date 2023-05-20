5c7c3fc9c242ec753969e666dc94e1b53e89cbde
Maurizio Oliveri <6tsukiyami9@gmail.com>
2023-05-20 12:13:59 +0200

    CHORE(backend): updated routes to work under the /api scope.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,26 +1,28 @@
 Rails.application.routes.draw do
 
-  api vendor_string: "fooffa",
-      default_version: 1,
-      path: "",
-      defaults: { format: :json } do
-    version 1 do
-      cache as: 'v1' do
-        # Login:
-        post "/login", to: "users#login"
+  scope :api do
+    api vendor_string: "scialla",
+        default_version: 1,
+        path: "",
+        defaults: { format: :json } do
+      version 1 do
+        cache as: 'v1' do
+          # Login:
+          post "/login", to: "users#login"
+        end
       end
     end
+  
+    # ActionCable:
+    mount ActionCable.server => "/cable"
+  
+    # Everything else returns a 404:
+    get "/*a", to: "application#not_found"
+    post "/*a", to: "application#not_found"
+  
+    # And the root is, you guessed, a 404:
+    root "application#not_found"
   end
-
-  # ActionCable:
-  mount ActionCable.server => "/cable"
-
-  # Everything else returns a 404:
-  get "/*a", to: "application#not_found"
-  post "/*a", to: "application#not_found"
-
-  # And the root is, you guessed, a 404:
-  root "application#not_found"
 
 end
 
